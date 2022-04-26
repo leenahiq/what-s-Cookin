deff0cbd5c474f84bf340ffe658a1aa923affb3e
Holly Christopher <holly_christopher@hotmail.com>
2022-04-26 14:34:33 +0100

    hc added api

---
+++ src/pages/api/Final.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import MealList from "./MealList";
+
+function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=b9c316981926453fa06eef522a721a00&timeFrame=day&targetCalories=${calories}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
+  return (
+    <div className="App">
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button>
+      </section>
+      {mealData && <MealList mealData={mealData} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/api/MealList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Meal from "./Meal";
+
+export default function MealList({ mealData }) {
+  const nutrients = mealData.nutrients;
+
+  return (
+    <main>
+      <section className="nutrients">
+        <h2>Daily Meal Planner</h2>
+        <h2>Macros</h2>
+        <ul>
+          <li>Calories: {nutrients.calories.toFixed(0)}</li>
+          <li>Carbohydrates: {nutrients.carbohydrates.toFixed(0)}</li>
+          <li>Fat: {nutrients.fat.toFixed(0)}</li>
+          <li>Protein: {nutrients.protein.toFixed(0)}</li>
+        </ul>
+      </section>
+
+      <section className="meals">
+        {mealData.meals.map((meal) => {
+          return <Meal key={meal.id} meal={meal} />;
+        })}
+      </section>
+    </main>
+  );
+}
