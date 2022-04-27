4ac12003d29135652b5e2838e65808aa075a4c23
leenahiq <97670794+leenahiq@users.noreply.github.com>
2022-04-27 13:47:17 +0100

    added recipe search bar by query and ingredient

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Getapi from "./pages/api/Final";
+import Ingredient from "./pages/ingredient";
+import Recipe from "./pages/recipe";
 
 function App() {
   return (
     <div className="App">
       <h1>main project</h1>
       <Getapi />
+      <Recipe />
+      <Ingredient />
     </div>
   );
 }
