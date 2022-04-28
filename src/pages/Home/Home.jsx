dd64329a0b804dcf81fd83a51b0e365a5f11b61a
Farzad <drfarghd@gmail.com>
2022-04-28 12:10:35 +0100

    Navbar and Router added by Farzad

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Popular from '../../components/popular';
 const Home = () => {
     return (
         <div className = 'container'>
-            <Popular/>
 
-            {/* <h1 className = 'text-center' style = {{paddingTop: '30%'}}>
+            <h1 className = 'text-center' style = {{paddingTop: '30%'}}>
                 Home
-            </h1> */}
+            </h1>
 
         </div>
     )
