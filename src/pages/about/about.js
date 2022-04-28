18ac6358f902bec36cb183dd606350f562008feb
Farzad <drfarghd@gmail.com>
2022-04-28 10:19:22 +0100

    Navbar and router added by Farzad

---
+++ src/pages/about/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const About = () => {
+    return (
+        <div className = 'container'>
+
+            <h1 className = 'text-center' style = {{paddingTop: '30%'}}>
+                This project is made by desk 4 of Code Nation. Contributors are Holy, Leenah, Ryan, and Farzad.
+            </h1>
+
+        </div>
+    )
+}
+
+export default About;
