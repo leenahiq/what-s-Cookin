ca340249eedb65ad08e9afbcff03645842b33d67
Holly Christopher <holly_christopher@hotmail.com>
2022-04-28 16:02:37 +0100

    hc css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Popular from "../../components/popular";
+
 const Home = () => {
   return (
     <div className="container">
-      <h1 className="text-center" style={{ paddingTop: "30%" }}>
+      <h1 
+      className="text-center" 
+      // style={{ paddingTop: "30%" }}
+      >
         <Popular />
       </h1>
     </div>
