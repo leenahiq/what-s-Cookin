d33424e465ce6ef4c2ab3a012b36bb12a2949de4
Farzad <drfarghd@gmail.com>
2022-05-03 18:15:16 +0100

    Logo added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import Popular from "../../components/popular";
 
 const Home = () => {
   return (
     <div className="container">
-      <h1 
-      className="text-center" 
-      // style={{ paddingTop: "30%" }}
-      >
-        <Popular />
-      </h1>
+  
     </div>
   );
 };
